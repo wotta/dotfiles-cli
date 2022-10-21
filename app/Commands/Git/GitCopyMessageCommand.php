5bc263b2501b318c9a90a22ddef64656fe28e8f0
Wouter <wouter.van.marrum@protonmail.com>
2022-10-22 00:07:15 +0200

    feature(GitCopyMessage): Made basic logic to fetch main remote branch.
    Worked on trying to get the commit messages based on the head branch and the remote branch.

--- app/Commands/Git/GitCopyMessageCommand.php
+++ app/Commands/Git/GitCopyMessageCommand.php
@@ -2,18 +2,22 @@
 
 namespace App\Commands\Git;
 
+use Illuminate\Support\Str;
+use App\Traits\InteractsWithProcess;
 use Illuminate\Console\Scheduling\Schedule;
+use JetBrains\PhpStorm\NoReturn;
 use LaravelZero\Framework\Commands\Command;
 
 class GitCopyMessageCommand extends Command
 {
+    use InteractsWithProcess;
+
     /**
      * The signature of the command.
      *
      * @var string
      */
-    protected $signature = 'git:copy:message
-                            {main=main : Git main branch (required)}';
+    protected $signature = 'git:copy:message';
 
     /**
      * The description of the command.
@@ -24,13 +28,36 @@ class GitCopyMessageCommand extends Command
 
     public function handle(): int
     {
+        $this->locateBinary('git');
+
+        $mainGitBranch = $this->runProcess([
+            'git',
+            'remote',
+            'show',
+            'origin'
+        ], true);
         
+
+        $mainBranch = $this->ask('Main branch', (string)Str::of($mainGitBranch)->after('HEAD branch:')->before("\n")->trim());
+
+        $commitMessages = $this->runProcess([
+            'git',
+            'log',
+            '--no-merged',
+            '--oneline',
+            '--decorate',
+            sprintf('%s..HEAD', $mainBranch),
+            '--pretty="format:%s"',
+        ]);
+
+        dd($commitMessages);
+        // git log --no-merges --oneline --decorate master..HEAD --pretty='format:%s'
     }
 
     /**
      * Define the command's schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     public function schedule(Schedule $schedule): void
